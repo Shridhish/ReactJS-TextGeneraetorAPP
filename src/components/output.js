812e3847aae6432d71f149cdcf1b12a6dd14dcb9
Shridhish R S <Shridhish7@gmail.com>
2018-02-28 20:41:59 -0500

    Add files via upload

---
+++ src/components/output.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+class output extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            value:props.value
+        }
+    }
+}
